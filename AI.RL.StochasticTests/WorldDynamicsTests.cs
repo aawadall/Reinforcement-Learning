5c874476781c223262e37c51a671b856b74b6908
Ahmed Awadallah <aawadall@ualberta.ca>
2017-08-07 19:09:24 -0400

    Further unit testing scaffolds

---
+++ AI.RL.StochasticTests/WorldDynamicsTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using AI.RL.Stochastic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.RL.Stochastic.Tests
+{
+    public class WorldDynamicsTests
+    {
+        [Fact()]
+        public void WorldDynamicsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ConstructTransTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ConstructRewardsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void InteractTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void InteractTest1()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetNextStateTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
