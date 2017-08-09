3a0ec20f057c19e5ac0847d097b4a228968be979
Ahmed Awadallah <aawadall@ualberta.ca>
2017-08-08 21:11:05 -0400

    Created a new class ActionCollection

---
+++ AI.RL.StochasticTests/ActionCollectionTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using AI.RL.Stochastic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.RL.Stochastic.Tests
+{
+    public class ActionCollectionTests
+    {
+        [Fact()]
+        public void ActionCollectionTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ActionCollectionTest1()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AddActionTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetActionAtTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
---
+++ code/ActionCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI.RL.Stochastic
+{
+    /*
+     * Action Collection is made of a set of actions, index and a general Collection Description
+     */
+    public class ActionCollection
+    {
+        private Action[] _actions;
+        private int _nActions;
+        private string _collectionName;
+
+        public ActionCollection():this("")
+        { }
+
+        public ActionCollection(string name)
+        {
+            _collectionName = name;
+        }
+
+        public Action[] Actions { get => _actions; }
+
+        public void AddAction(Action action)
+        {
+            Actions[_nActions++] = action;
+        }
+
+        public Action GetActionAt(int idx)
+        {
+            try
+            {
+                return Actions[idx];
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
